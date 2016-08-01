898dfddc30af26b540b4087633cbd950b7403ed1
Chekuda <sochekilla@gmail.com>
2016-08-01 15:01:29 +0100

    form saved if doesnt exist already

--- browser_side/widget.js
+++ browser_side/widget.js
@@ -12,6 +12,7 @@
 
 
 var listNewFormMappings=[];
+var veTag = "";
 
 function clearFields(data){
 
@@ -39,7 +40,7 @@ function removeItem(){
 
   for(var i=0;i<listNewFormMappings.length;i++)
   {
-    if(listNewFormMappings[i].formName == nodeToRemove)
+    if(listNewFormMappings[i].mappingName == nodeToRemove)
     {
       listNewFormMappings.splice(i,1);
     }
@@ -50,19 +51,18 @@ function removeItem(){
 
 function saveFormMapping(){
 
-  if(document.getElementById("mappingDataType").value != "" || document.getElementById("htmlType").value !="" || document.getElementById("htmlAttribute").value !="" || document.getElementById("fieldType").value !="" || document.getElementById("eventType").value !="")
-  {
-    var list=""; 
-    var newOne = {
-                mappingName: document.getElementById("mappingName").value,
-                mappingSelector: document.getElementById("mappingSelector").value,
-                mappingDataType: document.getElementById("mappingDataType").value,
-                htmlType: document.getElementById("htmlType").value,
-                htmlAttribute: document.getElementById("htmlAttribute").value,
-                fieldType: document.getElementById("fieldType").value,
-                eventType: document.getElementById("eventType").value
-              };
+var newOne = {
+      mappingName: document.getElementById("mappingName").value,
+      mappingSelector: document.getElementById("mappingSelector").value,
+      mappingDataType: document.getElementById("mappingDataType").value,
+      htmlType: document.getElementById("htmlType").value,
+      htmlAttribute: document.getElementById("htmlAttribute").value,
+      fieldType: document.getElementById("fieldType").value,
+      eventType: document.getElementById("eventType").value
+    };
 
+  if(newOne.mappingName!="" && newOne.mappingSelector!="" && newOne.mappingDataType!="" && newOne.htmlType!="" && newOne.htmlAttribute!="" && newOne.fieldType!="" && newOne.eventType!="")
+  {
     listNewFormMappings.push(newOne);
 
     var newDiv = document.createElement("div");
@@ -86,10 +86,91 @@ function saveFormMapping(){
 
   }
   else{
-    alert("Dont Forget the types");
+    alert("Dont Forget all data");
   }
 
 }
+
+
+function saveForm(){
+  var controlIfExist = "no";
+  if(listNewFormMappings.length<=0 || vetag == "" || document.getElementById("formName").value == "" || document.getElementById("formType").value =="")
+  {
+    if(vetag == "")
+    {
+      alert("The tag is not in the page");
+    }
+    else if(listNewFormMappings.length<=0)
+    {
+      alert("Please insert any form mapping");
+    }
+    else
+    {
+      alert("Please insert the Form name");
+    }
+  }
+  else
+  {
+    var ve_client=
+      {
+      journeyId: "",
+      form:[
+        {
+          formName:"",
+          formType:"",
+          formURL:"",
+          listMappings: listNewFormMappings
+        }]
+      };
+      ve_client.form[0].formName = document.getElementById("formName").value;
+      ve_client.form[0].formType =document.getElementById("formType").value;
+      ve_client.form[0].formURL = document.getElementById("wholeUrl").value;
+      ve_client.journeyId = vetag.match(/\b[A-Z0-9/]{2,}\b/)[0];
+      ve_client.journeyId = ve_client.journeyId.replace(/^\//,"");
+      ve_client.journeyId = ve_client.journeyId.replace(/\/$/,"");
+      ve_client.journeyId = ve_client.journeyId.replace(/\//g,"-");
+      console.log(ve_client.journeyId);
+      if(!window.localStorage.getItem("ve_widget"))
+      {
+        window.localStorage.setItem("ve_widget",JSON.stringify(ve_client));
+      }
+      else
+      {
+        var currentObject = JSON.parse(window.localStorage.getItem("ve_widget"));
+        
+        if(ve_client.journeyId == currentObject.journeyId)
+        {
+          for(var i=0;i<currentObject.form.length;i++)
+          {
+            if(currentObject.form[i].formName == ve_client.form[0].formName)
+            {
+              controlIfExist = i;
+            }
+          }
+          if(controlIfExist != "no")
+          {
+            currentObject.form[controlIfExist] = ve_client.form[0];
+            window.localStorage.setItem("ve_widget",JSON.stringify(currentObject));
+            alert("Form named>> "+ve_client.form[0].formName+" replaced");
+
+          }
+          else
+          {
+            currentObject.form.push(ve_client.form[0]);
+            window.localStorage.setItem("ve_widget",JSON.stringify(currentObject));
+            alert("Form named>> "+cve_client.form[0].formName+" added");
+          }
+          
+        }
+        else
+        {
+          alert("The client with JourneyId>> "+currentObject.journeyId+" is on your localStorage");
+        }
+      }
+      
+  }
+}
+
 function stopAlloldEvents(event)
 {
   event.stopPropagation();
@@ -189,10 +270,11 @@ function getSelector(event){
     {
         nodeClass = event.toElement.className;
 
-        if(nodeClass.match(/{{.+}}/))
+        if(nodeClass.match(/{{.+}}/))//remove the moustache elements 
         {
           nodeClass =  nodeClass.replace(nodeClass.match(/{{.+}}/)[0],"").trim();
         }
+        nodeClass = nodeClass.replace(/  /g," ");
         nodeClass = "."+nodeClass.replace(/ /g,".");
     }
     if(event.toElement.parentNode.id != "")
@@ -254,7 +336,8 @@ function checkveTag(){
       if(nScripts[i].src.match(/interactive\.com\/tags/))
       {
         console.log("VETAG>>>>>> "+nScripts[i].src);
-        document.querySelector("#vetag .iconOk").style.background = "url('chrome-extension://ojdefephjmdebknhoojenbpemafoeoga/assets/ok.png')";/*Loading the image from the extension URL*/
+        vetag = nScripts[i].src;
+        document.querySelector("#vetag .iconOk").style.background = "url('https://s25.postimg.org/vbbn1j7wf/ok.png')";/*Loading the image from the extension URL*/
         document.removeEventListener("DOMNodeInserted",checkveTag);
       }
     }
@@ -269,7 +352,7 @@ function checkveCapture()
       if(document.getElementById("veConnect"))
       {
           console.log("VECapture>>>>>> "+document.getElementById("veConnect").src);
-          document.querySelector("#vecapture .iconOk").style.background = "url('chrome-extension://ojdefephjmdebknhoojenbpemafoeoga/assets/ok.png')";/*Loading the image from the extension URL*/
+          document.querySelector("#vecapture .iconOk").style.background = "url('https://s25.postimg.org/vbbn1j7wf/ok.png')";/*Loading the image from the extension URL*/
 
       }
 
@@ -283,7 +366,9 @@ function calculateHeightofFormSection(){
 }
 
 function getWholeURl(){
-  document.getElementById("wholeUrl").value = window.location.hostname + window.location.pathname;
+  var wholeURL="";
+  wholeURL = window.location.hostname + window.location.pathname;
+  document.getElementById("wholeUrl").value = wholeURL.replace(/^www./g,"");
 }
 /********************
   Receive information from extension
@@ -306,6 +391,7 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
           document.querySelector("span.leftMoveIcon").addEventListener("click",moveLeftWidget);
           document.querySelector("span.rightMoveIcon").addEventListener("click",moveRightWidget);
           document.querySelector("#saveMapping").addEventListener("click",saveFormMapping);
+          document.querySelector("#saveForm").addEventListener("click",saveForm);
           checkveTag();
           checkveCapture();
           calculateHeightofFormSection();
